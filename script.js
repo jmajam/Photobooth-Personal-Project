bfd4c63582be2acf38f1604e809d3a463c64a460
Justin Majam <pmajam@my.bcit.ca>
2022-12-06 13:36:01 -0800

    implemented taking multiple photos

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let camera_button = document.querySelector("#start-camera");
 let video = document.querySelector("#video");
 let click_button = document.querySelector("#click-photo");
-let canvas = document.querySelector("#canvas");
+let canvas1 = document.querySelector("#canvas1");
+let canvases = document.getElementsByClassName("canvas");
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 camera_button.addEventListener('click', async function() {
    	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
@@ -9,9 +12,17 @@ camera_button.addEventListener('click', async function() {
 });
 
 click_button.addEventListener('click', function() {
-   	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-   	let image_data_url = canvas.toDataURL('image/jpeg');
-
-   	// data url of the image
-   	console.log(image_data_url);
+    insertImage();
 });
+
+async function insertImage(){
+    
+    for (var i = 0; i < canvases.length; i++) {
+        canvases[i].getContext('2d').drawImage(video, 0, 0, canvases[i].width, canvases[i].height);
+        let image_data_url = canvas1.toDataURL('image/jpeg');
+
+   	    // data url of the image
+   	    console.log(image_data_url);
+           await sleep(3000);
+    }
+}
