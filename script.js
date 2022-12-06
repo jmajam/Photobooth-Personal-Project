18e94c034a133acc6e59b92fbd6a45b7f993e280
Justin Majam <pmajam@my.bcit.ca>
2022-12-06 14:28:08 -0800

    started working on image merging

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let click_button = document.querySelector("#click-photo");
 let canvas1 = document.querySelector("#canvas1");
 let canvases = document.getElementsByClassName("canvas");
 
+import mergeImages from 'merge-images';
+
+
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 camera_button.addEventListener('click', async function() {
@@ -12,7 +15,14 @@ camera_button.addEventListener('click', async function() {
 });
 
 click_button.addEventListener('click', function() {
-    insertImage();
+    //insertImage();
+
+    mergeImages([
+        { src: 'body.png', x: 0, y: 0 },
+        { src: 'eyes.png', x: 32, y: 0 },
+        { src: 'mouth.png', x: 16, y: 0 }
+      ])
+        .then(b64 => console.log("f"));
 });
 
 async function insertImage(){
