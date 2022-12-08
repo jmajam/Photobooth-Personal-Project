e332d6ff2840dc2db424e33a73232275cb291f68
jmajam <54956224+jmajam@users.noreply.github.com>
2022-12-08 02:40:02 -0800

    cleaned up repo and code

--- src/script.js
+++ src/script.js
@@ -1,22 +1,30 @@
 let camera_button = document.querySelector("#start-camera");
 let video = document.querySelector("#video");
 let click_button = document.querySelector("#click-photo");
-let canvas1 = document.querySelector("#canvas1");
 let canvases = document.getElementsByClassName("canvas");
 
 import mergeImages from 'merge-images';
 
 let images = [];
+let lastFilename = "";
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 function makeFileName(){
+    let finalName = "";
     const date = new Date();
     let day = date.getDate();
     let month = date.getMonth() + 1;
     let year = date.getFullYear();
     let hours = date.getHours();
     let minutes = date.getMinutes();
-    let finalName = `${year}${month}${day}${hours}${minutes}`;
+    let seconds = date.getSeconds();
+    let name = `${year}${month}${day}${hours}${minutes}`;
+    //check if same name
+    if(name == lastFilename){
+        finalName = `${year}${month}${day}${hours}${minutes}${seconds}`;
+    } else {
+        finalName = `${year}${month}${day}${hours}${minutes}`;
+    }
     return finalName;
 
 }
@@ -47,6 +55,7 @@ click_button.addEventListener('click', async function() {
             {document.querySelector('img').src = b64;
             let name = makeFileName();
             download(b64,name);
+            lastFilename = name;
         });
     
 
