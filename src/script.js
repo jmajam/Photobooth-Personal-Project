b884a551b2a8011a131f13df80b3f5880304b9c6
jmajam <54956224+jmajam@users.noreply.github.com>
2022-12-08 01:15:33 -0800

    Got image merging with images taken to work

--- src/script.js
+++ src/script.js
@@ -4,9 +4,9 @@ let click_button = document.querySelector("#click-photo");
 let canvas1 = document.querySelector("#canvas1");
 let canvases = document.getElementsByClassName("canvas");
 
-import mergeImages from '../';
+import mergeImages from 'merge-images';
 
-
+let images = [];
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 camera_button.addEventListener('click', async function() {
@@ -14,15 +14,15 @@ camera_button.addEventListener('click', async function() {
 	video.srcObject = stream;
 });
 
-click_button.addEventListener('click', function() {
-    //insertImage();
-
+click_button.addEventListener('click', async function() {
+    await insertImage();
     mergeImages([
-        { src: 'body.png', x: 0, y: 0 },
+        { src: images[0], x: 0, y: 0 },
         { src: 'eyes.png', x: 32, y: 0 },
         { src: 'mouth.png', x: 16, y: 0 }
       ])
         .then(b64 => document.querySelector('img').src = b64);
+    
 
 });
 
@@ -31,9 +31,10 @@ async function insertImage(){
     for (var i = 0; i < canvases.length; i++) {
         canvases[i].getContext('2d').drawImage(video, 0, 0, canvases[i].width, canvases[i].height);
         let image_data_url = canvas1.toDataURL('image/jpeg');
-
+        images.push(image_data_url);
    	    // data url of the image
-   	    console.log(image_data_url);
-           await sleep(3000);
+        
+           await sleep(500);
     }
+    console.log(images.length);
 }
