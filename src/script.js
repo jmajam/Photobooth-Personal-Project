3c4f2764466434b0d87e148dfcf7194b8b5d896d
jmajam <54956224+jmajam@users.noreply.github.com>
2022-12-08 02:30:05 -0800

    got images to work with template
    added comments as well

--- src/script.js
+++ src/script.js
@@ -9,7 +9,27 @@ import mergeImages from 'merge-images';
 let images = [];
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
+function makeFileName(){
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let finalName = `${year}${month}${day}${hours}${minutes}`;
+    return finalName;
+
+}
+
+function download(dataurl, filename) {
+    const link = document.createElement("a");
+    link.href = dataurl;
+    link.download = filename;
+    link.click();
+}
+
 camera_button.addEventListener('click', async function() {
+    //get video stream
    	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
 	video.srcObject = stream;
 });
@@ -17,11 +37,17 @@ camera_button.addEventListener('click', async function() {
 click_button.addEventListener('click', async function() {
     await insertImage();
     mergeImages([
-        { src: images[0], x: 0, y: 0 },
-        { src: 'eyes.png', x: 32, y: 0 },
-        { src: 'mouth.png', x: 16, y: 0 }
+        { src: './images/christmas_background.png', x: 0, y: 0 },
+        { src: images[images.length-3], x: 960, y: 155 },
+        { src: images[images.length-2], x: 1722, y: 155 },
+        { src: images[images.length-1], x: 1040, y: 683 }
       ])
-        .then(b64 => document.querySelector('img').src = b64);
+        .then(b64 =>
+            //download image
+            {document.querySelector('img').src = b64;
+            let name = makeFileName();
+            download(b64,name);
+        });
     
 
 });
@@ -29,12 +55,13 @@ click_button.addEventListener('click', async function() {
 async function insertImage(){
     
     for (var i = 0; i < canvases.length; i++) {
+        //display images
         canvases[i].getContext('2d').drawImage(video, 0, 0, canvases[i].width, canvases[i].height);
-        let image_data_url = canvas1.toDataURL('image/jpeg');
+        let image_data_url = canvases[i].toDataURL('image/png');
         images.push(image_data_url);
    	    // data url of the image
         
-           await sleep(500);
+           await sleep(1000);
     }
     console.log(images.length);
 }
