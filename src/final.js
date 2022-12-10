812441db2a5f116fc7ac5882178d1219912bafee
jmajam <54956224+jmajam@users.noreply.github.com>
2022-12-10 04:40:58 -0800

    last styling change for the day

--- src/final.js
+++ src/final.js
@@ -1,4 +1,14 @@
 let img = document.getElementById("finalPreview");
+let end = document.getElementById("end");
+let change = document.getElementById("changeTemplate");
+
+end.addEventListener("click",() =>{
+    window.location ="booth.html"
+})
+
+change.addEventListener("click",() =>{
+    window.location ="../index.html"
+})
 
 function getImage(){
     let source = sessionStorage.getItem("final");
