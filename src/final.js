9206055ca96834da33e75b4a3809be0e4bbc6e12
jmajam <54956224+jmajam@users.noreply.github.com>
2022-12-14 19:08:22 -0800

    started email feature

--- src/final.js
+++ src/final.js
@@ -1,6 +1,7 @@
-let img = document.getElementById("finalPreview");
-let end = document.getElementById("end");
-let change = document.getElementById("changeTemplate");
+const img = document.getElementById("finalPreview");
+const end = document.getElementById("end");
+const change = document.getElementById("changeTemplate");
+const form = document.getElementById("emailForm");
 
 import {Base64String} from "./compress.js";
 
@@ -17,5 +18,29 @@ function getImage(){
     let sourceString = Base64String.decompressFromUTF16(output);
     img.src = 'data:image/png;base64,' + sourceString;
 }
+
+let modalBtn = [...document.querySelectorAll(".button")];
+modalBtn.forEach(function (btn) {
+    btn.onclick = function () {
+        let modal = btn.getAttribute("data-modal");
+        document.getElementById(modal).style.display = "block";
+    };
+});
+let closeBtns = [...document.querySelectorAll(".close")];
+closeBtns.forEach(function (btn) {
+    btn.onclick = function () {
+        let modal = btn.closest(".modal");
+        modal.style.display = "none";
+    };
+});
+window.onclick = function (event) {
+    if (event.target.className === "modal") {
+        event.target.style.display = "none";
+    }
+};
+
+form.addEventListener("submit", function (event) {
+    console.log(form.elements["emails"]);
+});
 
 getImage();
